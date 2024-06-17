678d2030d893d988b1779b50a891358230033283
Harry McCarney <harry@hackandcraft.com>
2024-06-17 20:32:07 +0100

    adding images and content

--- Program.fs
+++ Program.fs
@@ -5,19 +5,25 @@ module builder =
     open Suave
     open Suave.Filters
     open Suave.Operators
+    open System.IO
 
     let app = 
         choose [
-            GET >=> path "/" >=> Files.file "index.html"
+            GET >=> path "/" >=> Files.file "public/index.html"
+            GET >=> Files.browseHome            
+            RequestErrors.NOT_FOUND "Page not found." 
         ]
 
     [<EntryPoint>]
     let main args =
 
         Master.render
-        |> fun x -> System.IO.File.WriteAllText("index.html", x)
+        |> fun x -> System.IO.File.WriteAllText("public/index.html", x)
         |> ignore
 
+        let config =
+            {defaultConfig with homeFolder = Some (Path.GetFullPath "public") }
+            
         startWebServer defaultConfig app
         0
 
