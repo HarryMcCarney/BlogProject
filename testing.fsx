7c3f02d7dcaac22b9e807b5c9357ce9e1f0d31d6
Harry McCarney <hmccarney@gmail.com>
2024-06-19 22:08:13 +0100

    added foot and menu

--- testing.fsx
+++ testing.fsx
@@ -1,21 +1,95 @@
-#r "nuget: FSharp.Formatting, 20.0.1"
-#r "nuget: FSharp.Compiler.Service, 43.8.300"
-#r "nuget: FSharp.Formatting.Literate, 20.0.1"
 
-open System
-open System.IO
-open FSharp.Formatting.Literate
-open FSharp.Formatting.Markdown
-open FSharp.Formatting.Templating
-open FSharp.Formatting
-open FSharp.Compiler
+#r "nuget: Markdig, 0.37.0"
 
-
-//https://github.com/fsprojects/FSharp.Formatting/blob/4140311cf170c3704c69f5d1dff0bb6d4d35b069/tests/FSharp.Literate.Tests/LiterateTests.fs#L184
 
 open System.IO
+open Markdig
+open Markdig.Parsers
 
 let doc =  File.ReadAllText "testing.md" 
 
-let md = Literate.ParseMarkdownString(doc)
+let parsed = Markdig.Markdown.Parse doc
 
+Markdig.Markdown.
+
+Markdig.Markdown.ToHtml parsed
+
+parsed.
+
+
+
+(doc)
+
+
+
+
+open FSharp.Formatting.Markdown
+open FSharp.Formatting.Common
+
+
+
+let doc =  File.ReadAllText "testing.md" 
+
+let parsed = Markdown.Parse(doc)
+
+MarkdownParagraph.YamlFrontmatter
+
+parsed.Paragraphs
+|> List.filter(fun p -> 
+    match p with 
+    | HorizontalRule (character, range)-> true
+    | _ -> false  
+    )
+|> List.take 2
+
+// get first and second 
+
+printfn "%A" parsed.Paragraphs 
+
+for par in parsed.Paragraphs do
+
+    match par with
+    | Heading (size = 2; body = [ Literal (text = text) ]) ->
+        // Recognize heading that has a simple content
+        // containing just a literal (no other formatting)
+        printfn "%s" text
+    | Paragraph (spans, _) -> 
+        spans
+        |> List.iter(fun s -> 
+            match s with 
+            | Paragraph (body = [ Literal (text = text) ]) -> 
+                printfn "%s" text
+            | DirectLink (body = [ Literal (link = link)]) -> 
+                printfn "%s" link
+        
+        )
+    | _ -> ()
+    |> ignore
+
+
+let rec collectSpanLinks span =
+    seq {
+        match span with
+        | DirectLink (link = url) -> yield url
+        | IndirectLink (key = key) -> yield fst (parsed.DefinedLinks.[key])
+        | MarkdownPatterns.SpanLeaf _ -> ()
+        | MarkdownPatterns.SpanNode (_, spans) ->
+            for s in spans do
+                yield! collectSpanLinks s
+    }
+
+/// Returns all links in the specified paragraph node
+let rec collectParLinks par =
+    seq {
+        match par with
+        | MarkdownPatterns.ParagraphLeaf _ -> ()
+        | MarkdownPatterns.ParagraphNested (_, pars) ->
+            for ps in pars do
+                for p in ps do
+                    yield! collectParLinks p
+        | MarkdownPatterns.ParagraphSpans (_, spans) ->
+            for s in spans do
+                yield! collectSpanLinks s
+    }
+
+Seq.collect collectParLinks parsed.Paragraphs
