a3f8e20ca9782786dc7597cb37bbd672af15a607
Harry McCarney <hmccarney@gmail.com>
2024-06-23 16:21:30 +0100

    added fable and hamburger menu

---
+++ Javascript/Script.fs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+open Browser.Dom
+open Fable.Core
+open Fable.Core.JsInterop
+
+let expandHamburger() =
+    let burger = document.getElementById("navbar-burger")
+    let menu = document.getElementById("navbarBasicExample")
+
+    burger.addEventListener("click", fun _ ->
+        burger.classList.toggle("is-active") 
+        |> ignore
+        menu.classList.toggle("is-active") 
+        |> ignore 
+    )
+
+
+[<Emit("window.execScripts = $0")>]
+let exportexecScripts (greet: obj) = jsNative
+
+let execScripts() =
+    expandHamburger()
+
+exportexecScripts execScripts
+
+
+
+(*
+function expandHamburger() {
+    const burger = document.getElementById("navbar-burger");
+    const menu = document.getElementById("navbarBasicExample");
+    burger.addEventListener("click", (_arg) => {
+        burger.classList.toggle("is-active");
+        menu.classList.toggle("is-active");
+    });
+}
+*)
