e3c0e1f80e1f62cd37a0b24643c74a3cb6ab1288
Harry McCarney <harry@hackandcraft.com>
2024-08-05 15:47:01 +0100

    deploying to github pages

--- Model/obj/Debug/net7.0/Model.AssemblyInfo.fs
+++ Model/obj/Debug/net7.0/Model.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Model")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4fb586bfca587f0b295a2a97cf08bb77b4ae962")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbeafbba4caee43439c80810de4079ce90be7cc")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Model")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Model")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- Model/obj/Release/net7.0/Model.fable-temp.AssemblyInfo.cs
+++ Model/obj/Release/net7.0/Model.fable-temp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model.fable-temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4fb586bfca587f0b295a2a97cf08bb77b4ae962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbeafbba4caee43439c80810de4079ce90be7cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model.fable-temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model.fable-temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
