e0d5ee1ed6ed4016329f64680cdf8bc1aa4ceb3e
Harry McCarney <harry@hackandcraft.com>
2024-08-12 10:47:54 +0100

    deploying to github pages

--- Model/obj/Debug/net7.0/Model.AssemblyInfo.fs
+++ Model/obj/Debug/net7.0/Model.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Model")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c0e1f80e1f62cd37a0b24643c74a3cb6ab1288")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d848c75994642d296cdfd1a522035b548f55b2")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Model")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Model")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
